61feb266e4ab929f50a2411ecbfabd242e7e1637
John <john@agencysocial.com>
2026-08-14 19:47:55 -0400

    Remove debug logging from applyAngle

--- app/render.py
+++ app/render.py
@@ -2501,9 +2501,8 @@ function getAngles(cid) {{
   }});
 }}
 function applyAngle(cid, idx) {{
-  console.log('applyAngle called cid='+cid+' idx='+idx+' stored='+JSON.stringify(_ws_angles));
   var a = (_ws_angles[cid]||[])[idx];
-  if (!a) {{ console.error('applyAngle: no angle found. cid='+cid+' idx='+idx+' keys='+Object.keys(_ws_angles)+' len='+((_ws_angles[cid]||[]).length)); return; }}
+  if (!a) return;
   var hlEl = document.getElementById('draft-hl-'+cid);
   var tagEl = document.getElementById('draft-tag-'+cid);
   var scEl  = document.getElementById('draft-scene-'+cid);
